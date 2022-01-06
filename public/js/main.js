d1a3451368fb334c058c09c9ede9f90b8b376f6f
javierdiezm <javier.diez@alumnos.uneatlantico.es>
2022-01-06 17:31:42 +0100

    pwa iii

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,15 @@ const socket = io();
 //  join chatroom
 socket.emit('joinRoom', { username, room });
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
 //  get room and users
 socket.on('roomUsers', ({ room, users }) => {
   outputRoomName(room);
